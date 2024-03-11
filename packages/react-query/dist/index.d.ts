7c627e2388e25280d0276ed3c45e672802542f5f
Matthieu Hocquart <matthieu@hocquart.fr>
2024-03-11 23:21:05 +0100

    Release

---
+++ packages/react-query/dist/index.d.ts
@@ -0,0 +1,6 @@
+export * from '@trpc/client';
+export { getQueryKey } from './internals/getQueryKey';
+export { createTRPCReact, type CreateTRPCReact, type CreateTRPCReactBase, } from './createTRPCReact';
+export { createReactQueryHooks } from './interop';
+export type { inferReactQueryProcedureOptions } from './utils/inferReactQueryProcedure';
+//# sourceMappingURL=index.d.ts.map
